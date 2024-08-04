3e0e3fa43d108a4cdae5b6d70213ca00881eb6a1
DavydenkoA <davydenko.daf80@yandex.ru>
2024-08-04 11:36:46 +0300

    Add files via upload
    Запуск браузера в скрытом (или "--headless") режиме с расширением — это не совсем тривиальная задача, которая может быть полезна для различных сценариев веб-автоматизации или парсинга. Обычно расширения используются для модификации поведения браузера, например, для блокировки рекламы, изменения пользовательского агента или управления cookies.
    
    И если вам потребуется запустить браузер с расширениями  и в режиме --headless, то необходимо прописать options.add_argument("--headless=chrome")

---
+++ Запуск_браузера_в_скрытом_режиме.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=chrome')
+options.add_extension('coordinates.crx')
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_argument('--headless=chrome')
+options_chrome.add_extension('coordinates.crx')
+
+with webdriver.Chrome(options=options_chrome) as browser:
+    url = 'https://yandex.ru/'
+    browser.get(url)
+    time.sleep(5)
+    a = browser.find_element(By.TAG_NAME, 'a')
+    print(a.get_attribute('href'))
